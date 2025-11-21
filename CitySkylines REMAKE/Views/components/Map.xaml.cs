770c0cc4cc28d0c78cb324a3013021a4ddd451a5
dnl05 <danek2005w@gmail.com>
2025-11-21 16:15:30 +0300

    Добавлен зум карты через ctrl

--- CitySkylines REMAKE/Views/components/Map.xaml.cs
+++ CitySkylines REMAKE/Views/components/Map.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CitySkylines_REMAKE.Views.components
 {
@@ -20,9 +11,80 @@ namespace CitySkylines_REMAKE.Views.components
     /// </summary>
     public partial class Map : UserControl
     {
+        // Поля для логики перетаскивания
+        private Point? _lastMousePosition;
+        private bool _isDragging = false;
+
         public Map()
         {
             InitializeComponent();
+        }
+
+        // --- ЗУМ КАРТЫ ---
+        private void MapScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (Keyboard.Modifiers != ModifierKeys.Control) return;
+            var scale = e.Delta > 0 ? 1.1 : 0.9;
+            ScaleTransform.ScaleX *= scale;
+            ScaleTransform.ScaleY *= scale;
+            e.Handled = true;
+        }
+
+        // --- ПЕРЕТАСКИВАНИЕ (ПКМ) ---
+        private void MapScrollViewer_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Начинаем перетаскивание только по Правой Кнопке Мыши
+            if (e.ChangedButton == MouseButton.Right)
+            {
+                if (e.Handled) return;
+
+                // Сохраняем начальную позицию мыши относительно UserControl
+                _lastMousePosition = e.GetPosition(this);
+                _isDragging = true;
+
+                MapScrollViewer.CaptureMouse();
+                MapScrollViewer.Cursor = Cursors.SizeAll;
+                e.Handled = true;
+            }
+        }
+
+        // --- ПЕРЕМЕЩЕНИЯ (ДРАГ) ---
+        private void MapScrollViewer_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Если активно перетаскивание
+            if (_isDragging && _lastMousePosition.HasValue)
+            {
+                Point currentPosition = e.GetPosition(this);
+
+                Vector delta = currentPosition - _lastMousePosition.Value;
+
+                // Применяем смещение к TranslateTransform
+                TranslateTransform.X += delta.X;
+                TranslateTransform.Y += delta.Y;
+
+                _lastMousePosition = currentPosition;
+                MapScrollViewer.Cursor = Cursors.SizeAll;
+            }
+            // В неактивном режиме перетаскивания курсор должен быть стрелкой
+            else if (!_isDragging)
+            {
+                MapScrollViewer.Cursor = Cursors.Arrow;
+            }
+        }
+
+        // --- ЗАВЕРШЕНИЕ ПЕРЕТАСКИВАНИЯ ---   
+        private void MapScrollViewer_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            // Завершаем перетаскивание только если оно было активно
+            if (_isDragging && e.ChangedButton == MouseButton.Right)
+            {
+                _isDragging = false;
+                _lastMousePosition = null;
+
+                MapScrollViewer.ReleaseMouseCapture();
+                MapScrollViewer.Cursor = Cursors.Arrow;
+                e.Handled = true;
+            }
         }
     }
 }
