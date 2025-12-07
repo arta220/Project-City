80b870f79f3fb5f9b3a7d27f5ee567173a34b32b
Иван Суворов <fffffddddddqwrt@gmail.com>
2025-12-07 17:21:50 +0300

    Добавлено немного тестов

---
+++ Tests/Mocks/FakeUtilityService.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Buildings.Residential;
+using Domain.Common.Enums;
+using Domain.Common.Time;
+using Services.Utilities;
+using System.Collections.Generic;
+
+namespace Tests.Mocks
+{
+    public class FakeUtilityService : IUtilityService
+    {
+        private readonly Dictionary<ResidentialBuilding, HashSet<UtilityType>> _broken = new();
+
+        public void BreakUtilityForTesting(ResidentialBuilding building, UtilityType utilityType, int currentTick)
+        {
+            if (!_broken.ContainsKey(building))
+                _broken[building] = new HashSet<UtilityType>();
+            _broken[building].Add(utilityType);
+        }
+
+        public void FixAllUtilities(ResidentialBuilding building) => _broken.Remove(building);
+
+        public void FixUtility(ResidentialBuilding building, UtilityType utilityType)
+        {
+            if (_broken.ContainsKey(building))
+                _broken[building].Remove(utilityType);
+        }
+
+        public Dictionary<UtilityType, int> GetBrokenUtilities(ResidentialBuilding building)
+        {
+            if (!_broken.ContainsKey(building)) return new Dictionary<UtilityType, int>();
+            var dict = new Dictionary<UtilityType, int>();
+            foreach (var ut in _broken[building])
+                dict[ut] = 1;
+            return dict;
+        }
+
+        public UtilityStatistics GetStatistics() => new UtilityStatistics();
+
+        public void Update() { }
+
+        public void Update(SimulationTime time)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
