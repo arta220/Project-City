d861a438435ba03d613e2ea7d498e6f2a04715a7
Иван Суворов <fffffddddddqwrt@gmail.com>
2025-12-11 13:36:38 +0300

    Правки WorkScenario

--- Services/Citizens/Scenarios/Work/WorkScenario.cs
+++ Services/Citizens/Scenarios/Work/WorkScenario.cs
@@ -1,40 +1,44 @@
-﻿using Domain.Citizens;
+﻿using Domain.Buildings;
+using Domain.Citizens;
 using Domain.Citizens.Tasks;
+using Domain.Common.Base;
 using Services.BuildingRegistry;
+using Services.Citizens.Scenarios.Base;
 using Services.Citizens.Tasks.Job;
 using Services.EntityMovement.Service;
 using Services.Time;
 
-namespace Services.Citizens.Scenarios.Work;
-public class WorkScenario : MoveAndPerformTaskScenario
+namespace Services.Citizens.Scenarios.Work
 {
-    public WorkScenario(IEntityMovementService movement, IBuildingRegistry registry)
-        : base(movement, registry)
+    public class WorkScenario : MoveAndPerformTaskScenario
     {
-    }
-
-    public override bool CanRun(Citizen citizen, ISimulationTimeService time)
-    {
-        return citizen.Profession != null;
-    }
-
-    public override void BuildTasks(Citizen citizen)
-    {
-        if (citizen.WorkPlace == null)
+        public WorkScenario(IEntityMovementService movement, IBuildingRegistry registry)
+            : base(movement, registry)
         {
-            // Сначала задача найти работу
-            citizen.Tasks.Enqueue(new FindJobTask(_registry));
         }
 
-        // Если работа найдена — перемещение и выполнение работы
-        if (citizen.WorkPlace != null)
+        public override bool CanRun(Citizen citizen, ISimulationTimeService time)
         {
-            base.BuildTasks(citizen); // Создаёт PerformWorkTask через MoveAndPerformTaskScenario
+            // Работает только если гражданин трудоспособен,
+            // но WorkPlace может быть пока не назначен
+            return citizen.Profession != null;
         }
-    }
 
-    protected override ICitizenTask CreateMainTask(Citizen citizen)
-    {
-        return new PerformWorkTask(citizen);
+        public override void BuildTasks(Citizen citizen)
+        {
+            // 1. Если нет рабочего места — ищем работу
+            if (citizen.WorkPlace == null)
+            {
+                citizen.Tasks.Enqueue(new FindJobTask(_registry));
+                return;
+            }
+
+            // 2. Если работа есть — выполняем обычный MovePerformWork
+            base.BuildTasks(citizen);
+        }
+
+        protected override Building GetTargetBuilding(Citizen citizen) => citizen.WorkPlace!;
+        protected override ICitizenTask CreateMainTask(Citizen citizen, Building target) => new PerformWorkTask(citizen);
+        protected override Building? GetReturnTarget(Citizen citizen) => null; // Можно куда-то вернуться, но удобнее будет в другом сценарии
     }
 }
