cf93030a7f2fd9ce588e8985bb96faa3f9710b11
zxcmaxzxc <wirelessgtx@mail.ru>
2025-12-12 16:54:48 +0300

    Update IndustrialProductionService.cs

--- Services/IndustrialProduction/IndustrialProductionService.cs
+++ Services/IndustrialProduction/IndustrialProductionService.cs
@@ -16,6 +16,12 @@ namespace Services.IndustrialProduction
         private int _totalPackagingProduction = 0;
         private int _totalCardboardMaterialsUsed = 0;
         private int _totalPackagingMaterialsUsed = 0;
+         // Статистика сельского хозяйства
+        private int _totalAgricultureProduction = 0;
+        private int _totalAgricultureMaterialsUsed = 0;
+        // Статистика рыбодобывающей отрасли
+        private int _totalFishingProduction = 0;
+        private int _totalFishingMaterialsUsed = 0;
 
         public IndustrialProductionService(IBuildingRegistry buildingRegistry)
         {
@@ -30,6 +36,12 @@ namespace Services.IndustrialProduction
             int packagingProduction = 0;
             int cardboardMaterialsUsed = 0;
             int packagingMaterialsUsed = 0;
+             // Статистика сельского хозяйства
+            int agricultureProduction = 0;
+            int agricultureMaterialsUsed = 0;
+            // Статистика рыбодобывающей отрасли
+            int fishingProduction = 0;
+            int fishingMaterialsUsed = 0;
 
             foreach (var building in industrialBuildings)
             {
@@ -50,6 +62,20 @@ namespace Services.IndustrialProduction
 
                 packagingProduction += packagingProducts;
 
+                 // Подсчёт производства сельскохозяйственной продукции
+                var agricultureProducts = building.ProductsBank
+                    .Where(kvp => IsAgricultureProduct(kvp.Key))
+                    .Sum(kvp => kvp.Value);
+
+                agricultureProduction += agricultureProducts;
+
+                // Подсчёт производства рыбодобывающей отрасли
+                var fishingProducts = building.ProductsBank
+                    .Where(kvp => IsFishingProduct(kvp.Key))
+                    .Sum(kvp => kvp.Value);
+
+                fishingProduction += fishingProducts;
+
                 // Подсчёт использованных материалов для картона
                 var cardboardMaterials = building.MaterialsBank
                     .Where(kvp => IsCardboardMaterial(kvp.Key))
@@ -63,6 +89,20 @@ namespace Services.IndustrialProduction
                     .Sum(kvp => kvp.Value);
 
                 packagingMaterialsUsed += packagingMaterials;
+
+                // Подсчёт использованных материалов для сельского хозяйства
+                var agricultureMaterials = building.MaterialsBank
+                    .Where(kvp => IsAgricultureMaterial(kvp.Key))
+                    .Sum(kvp => kvp.Value);
+
+                agricultureMaterialsUsed += agricultureMaterials;
+
+                // Подсчёт использованных материалов для рыбодобывающей отрасли
+                var fishingMaterials = building.MaterialsBank
+                    .Where(kvp => IsFishingMaterial(kvp.Key))
+                    .Sum(kvp => kvp.Value);
+
+                fishingMaterialsUsed += fishingMaterials;
             }
 
             // Обновление статистики
@@ -70,6 +110,10 @@ namespace Services.IndustrialProduction
             _totalPackagingProduction += packagingProduction;
             _totalCardboardMaterialsUsed += cardboardMaterialsUsed;
             _totalPackagingMaterialsUsed += packagingMaterialsUsed;
+            _totalAgricultureProduction += agricultureProduction;
+            _totalAgricultureMaterialsUsed += agricultureMaterialsUsed;
+            _totalFishingProduction += fishingProduction;
+            _totalFishingMaterialsUsed += fishingMaterialsUsed;
 
             // Добавление точки данных
             var dataPoint = new ProductionDataPoint(
@@ -83,6 +127,25 @@ namespace Services.IndustrialProduction
             _statistics.CardboardHistory.Add(dataPoint);
             _statistics.PackagingHistory.Add(dataPoint);
         }
+        
+          // Добавление точки данных для сельского хозяйства
+            var agricultureDataPoint = new ProductionDataPoint(
+                time.TotalTicks,
+                agricultureProduction,
+                agricultureMaterialsUsed
+            );
+
+            _statistics.AgricultureHistory.Add(agricultureDataPoint);
+
+            // Добавление точки данных для рыбодобывающей отрасли
+            var fishingDataPoint = new ProductionDataPoint(
+                time.TotalTicks,
+                fishingProduction,
+                fishingMaterialsUsed,
+                true
+            );
+
+            _statistics.FishingHistory.Add(fishingDataPoint);
 
         public IndustrialProductionStatistics GetStatistics() => _statistics;
 
@@ -148,6 +211,83 @@ namespace Services.IndustrialProduction
             {
                 return productType == ProductType.CardboardSheets ||
                        productType == ProductType.Plastic;
+            }
+            return false;
+        }
+        
+        /// <summary>
+        /// Проверяет, является ли продукт сельскохозяйственной продукцией
+        /// </summary>
+        private bool IsAgricultureProduct(object product)
+        {
+            if (product is ProductType productType)
+            {
+                return productType == ProductType.Crops ||
+                       productType == ProductType.Vegetables ||
+                       productType == ProductType.Fruits ||
+                       productType == ProductType.Grains ||
+                       productType == ProductType.DairyProducts ||
+                       productType == ProductType.Meat ||
+                       productType == ProductType.Eggs ||
+                       productType == ProductType.ProcessedFood;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Проверяет, является ли продукт продукцией рыбодобывающей отрасли
+        /// </summary>
+        private bool IsFishingProduct(object product)
+        {
+            if (product is ProductType productType)
+            {
+                return productType == ProductType.ProcessedFish ||
+                       productType == ProductType.CannedFish ||
+                       productType == ProductType.FrozenFish ||
+                       productType == ProductType.FishProducts ||
+                       productType == ProductType.Fishmeal;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Проверяет, является ли материал ресурсом для сельского хозяйства
+        /// </summary>
+        private bool IsAgricultureMaterial(object material)
+        {
+            if (material is NaturalResourceType resourceType)
+            {
+                return resourceType == NaturalResourceType.Seeds ||
+                       resourceType == NaturalResourceType.Fertilizer ||
+                       resourceType == NaturalResourceType.Feed ||
+                       resourceType == NaturalResourceType.Livestock ||
+                       resourceType == NaturalResourceType.Chemicals ||
+                       resourceType == NaturalResourceType.Water ||
+                       resourceType == NaturalResourceType.Energy;
+            }
+            if (material is ProductType productType)
+            {
+                return productType == ProductType.Crops;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Проверяет, является ли материал ресурсом для рыбодобывающей отрасли
+        /// </summary>
+        private bool IsFishingMaterial(object material)
+        {
+            if (material is NaturalResourceType resourceType)
+            {
+                return resourceType == NaturalResourceType.Fish ||
+                       resourceType == NaturalResourceType.Ice ||
+                       resourceType == NaturalResourceType.FuelForFleet ||
+                       resourceType == NaturalResourceType.Energy ||
+                       resourceType == NaturalResourceType.Water;
+            }
+            if (material is ProductType productType)
+            {
+                return productType == ProductType.ProcessedFish;
             }
             return false;
         }
