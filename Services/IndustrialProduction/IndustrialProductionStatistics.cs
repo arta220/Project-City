b92d2099bdeef0e73f4c647f63f3d685f0068dd1
zxcmaxzxc <wirelessgtx@mail.ru>
2025-12-12 16:50:18 +0300

    Update IndustrialProductionStatistics.cs

--- Services/IndustrialProduction/IndustrialProductionStatistics.cs
+++ Services/IndustrialProduction/IndustrialProductionStatistics.cs
@@ -12,6 +12,12 @@ namespace Services.IndustrialProduction
         public int PackagingProduction { get; set; }
         public int CardboardMaterialsUsed { get; set; }
         public int PackagingMaterialsUsed { get; set; }
+         // Статистика сельского хозяйства
+        public int AgricultureProduction { get; set; }
+        public int AgricultureMaterialsUsed { get; set; }
+        // Статистика рыбодобывающей отрасли
+        public int FishingProduction { get; set; }
+        public int FishingMaterialsUsed { get; set; }
 
         public ProductionDataPoint(int tick, int cardboardProduction, int packagingProduction, 
             int cardboardMaterialsUsed, int packagingMaterialsUsed)
@@ -24,6 +30,22 @@ namespace Services.IndustrialProduction
         }
     }
 
+    // Конструктор для сельского хозяйства
+        public ProductionDataPoint(int tick, int agricultureProduction, int agricultureMaterialsUsed)
+        {
+            Tick = tick;
+            AgricultureProduction = agricultureProduction;
+            AgricultureMaterialsUsed = agricultureMaterialsUsed;
+        }
+
+        // Конструктор для рыбодобывающей отрасли
+        public ProductionDataPoint(int tick, int fishingProduction, int fishingMaterialsUsed, bool isFishing = true)
+        {
+            Tick = tick;
+            FishingProduction = fishingProduction;
+            FishingMaterialsUsed = fishingMaterialsUsed;
+        }
+
     /// <summary>
     /// Статистика производства промышленных зданий
     /// </summary>
@@ -31,6 +53,8 @@ namespace Services.IndustrialProduction
     {
         public List<ProductionDataPoint> CardboardHistory { get; set; } = new();
         public List<ProductionDataPoint> PackagingHistory { get; set; } = new();
+        public List<ProductionDataPoint> AgricultureHistory { get; set; } = new();
+        public List<ProductionDataPoint> FishingHistory { get; set; } = new();
     }
 }
 
